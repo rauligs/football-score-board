dcf5a9cea47ddfd33b89db0b0cc10b9aeb38a0ef
Raul Garcia <raul.garcia@thenesor.com>
2021-04-07 11:52:16 +0100

    add title content test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <span data-testid="titleText"></span>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('displays app with expected title text', async () => {
+  render(<App />);
+  const titleElement = await screen.findByTestId("titleText");
+  expect(titleElement.textContent).toBe("Football Score Board");
+});
