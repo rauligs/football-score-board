e940cafb933a195ff48a23843483d3ebe0840911
Raul Garcia <raul.garcia@thenesor.com>
2021-04-09 11:05:59 +0100

    Add game feature test

--- cypress/integration/score_board.spec.js
+++ cypress/integration/score_board.spec.js
@@ -9,4 +9,12 @@ describe('Score Board', () => {
         cy.visit('http://localhost:3000');
         cy.get('[data-testid="scoreBoardApp"]').should('be.visible');
     });
+
+    it('Should add a game and get displayed', () => {
+        cy.visit('http://localhost:3000');
+        cy.get('#home-team').type('Team A')
+        cy.get('#away-team').type('Team B')
+        cy.get('#start-game-form').submit()
+        cy.get('#summary-games-table tr').find('td').contains('Team A 0 - Team B 0')
+    })
 });
